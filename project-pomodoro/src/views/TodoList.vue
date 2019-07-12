a5f6a42e4ac22126db91e6ec1a056ec25941713b
Eason Liu <a0956031116@gmail.com>
2019-07-12 08:44:09 +0800

    add jQuery

--- project-pomodoro/src/views/TodoList.vue
+++ project-pomodoro/src/views/TodoList.vue
@@ -13,42 +13,48 @@
     <ul class="mission-list-container">
       <li class="mission-title">
         <span>TO-DO</span>
-        <a href="#" @click.prevent class="btn-dropdown"></a>
+        <a href="#" @click.prevent="todoMissionDropToggle"
+        class="btn-dropdown" id="btn-todo-dropdown"></a>
       </li>
-      <li class="missions" v-for="(item, key) in missions" :key="key" v-show="!item.completed">
-        <input class="mission-selector" :id="item.id" name="mission-selector"
-        type="checkbox"/>
-        <label class="mission-text" :for="item.id"
-        :class="{'line-through':item.completed}" @click="toggleComplete(item)">
-          <span></span>
-          {{item.missionTitle}}
-        </label>
-        <div class="btn-group">
-          <a href="#" class="btn-edit mission-btn" @click.prevent></a>
-          <a href="#" class="btn-delete mission-btn" @click.prevent="deleteMission(item)">×</a>
-          <a href="#" class="btn-start-count mission-btn" @click.prevent></a>
-        </div>
-      </li>
+      <div id="todo-missions">
+        <li class="missions" v-for="(item, key) in missions" :key="key" v-show="!item.completed">
+          <input class="mission-selector" :id="item.id" name="mission-selector"
+          type="checkbox"/>
+          <label class="mission-text" :for="item.id"
+          :class="{'line-through':item.completed}" @click="toggleComplete(item)">
+            <span></span>
+            {{item.missionTitle}}
+          </label>
+          <div class="btn-group">
+            <a href="#" class="btn-edit mission-btn" @click.prevent></a>
+            <a href="#" class="btn-delete mission-btn" @click.prevent="deleteMission(item)">×</a>
+            <a href="#" class="btn-start-count mission-btn" @click.prevent></a>
+          </div>
+        </li>
+      </div>
     </ul>
     <ul class="mission-list-container">
       <li class="mission-title">
         <span>DONE</span>
-        <a href="#" @click.prevent="dropDown" class="btn-dropdown"></a>
+        <a href="#" @click.prevent="doneMissionDropToggle"
+        class="btn-dropdown" id="btn-done-dropdown"></a>
       </li>
-      <li class="missions" v-for="(item, key) in missions" :key="key" v-show="item.completed">
-        <input class="mission-selector" :id="item.id" name="mission-selector"
-        type="checkbox"/>
-        <label class="mission-text" :for="item.id"
-        :class="{'line-through':item.completed}" @click="toggleComplete(item)">
-          <span :class="{'fake-checked': item.completed === true}"></span>
-          {{item.missionTitle}}
-        </label>
-        <div class="btn-group">
-          <a href="#" class="btn-edit mission-btn" @click.prevent></a>
-          <a href="#" class="btn-delete mission-btn" @click.prevent="deleteMission(item)">×</a>
-          <a href="#" class="btn-start-count mission-btn" @click.prevent></a>
-        </div>
-      </li>
+      <div id="done-missions">
+        <li class="missions" v-for="(item, key) in missions" :key="key" v-show="item.completed">
+          <input class="mission-selector" :id="item.id" name="mission-selector"
+          type="checkbox"/>
+          <label class="mission-text" :for="item.id"
+          :class="{'line-through':item.completed}" @click="toggleComplete(item)">
+            <span :class="{'fake-checked': item.completed === true}"></span>
+            {{item.missionTitle}}
+          </label>
+          <div class="btn-group">
+            <a href="#" class="btn-edit mission-btn" @click.prevent></a>
+            <a href="#" class="btn-delete mission-btn" @click.prevent="deleteMission(item)">×</a>
+            <a href="#" class="btn-start-count mission-btn" @click.prevent></a>
+          </div>
+        </li>
+      </div>
     </ul>
   </div>
 </template>
@@ -56,6 +62,7 @@
 <script>
 import { mapFields } from 'vuex-map-fields';
 import { mapGetters, mapActions } from 'vuex';
+import $ from 'jquery';
 
 export default {
   computed: {
@@ -75,6 +82,14 @@ export default {
     },
     deleteMission(el) {
       this.$store.dispatch('deleteMission', el);
+    },
+    todoMissionDropToggle() {
+      $('#todo-missions').fadeToggle();
+      $('#btn-todo-dropdown').toggleClass('turn-opposite');
+    },
+    doneMissionDropToggle() {
+      $('#done-missions').fadeToggle();
+      $('#btn-done-dropdown').toggleClass('turn-opposite');
     },
   },
 };
