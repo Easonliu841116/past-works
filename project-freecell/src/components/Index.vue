10024947e5b52fd5dd3c401b118693bb97ccf0ae
Eason Liu <a0956031116@gmail.com>
2019-07-23 01:35:28 +0800

    add freecell

---
+++ project-freecell/src/components/Index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="c-container mx-auto">
+      <div class="c-slot-container mx-auto">
+        <div class="c-slot-panel" id="blank-slot">
+          <div class="c-blank c-slot" v-for="(item, key) in 4" :key="key">
+              1
+          </div>
+        </div>
+        <div class="c-slot-panel" id="target-slot">
+          <div class="c-target c-slot" v-for="(item, key) in 4" :key="key">
+              2
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style lang="scss">
+</style>
