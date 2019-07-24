c303874133618d91594b008192a83dfceb48a6df
Eason Liu <a0956031116@gmail.com>
2019-07-25 01:32:26 +0800

    add deal function

--- project-freecell/src/components/Index.vue
+++ project-freecell/src/components/Index.vue
@@ -45,6 +45,52 @@
 
 <script>
 export default {
+  data() {
+    return {
+      decks: {
+        base: [[], [], [], [], [], [], [], []],
+        blank: [[], [], [], []],
+        target: [[], [], [], []],
+      },
+    };
+  },
+  methods: {
+    // 以 7/6 的方式發牌，牌的內容不重複
+    deal() {
+      const vm = this;
+      const tempDeck = [...vm.shuffledCard];
+      vm.decks.base.map((el, index) => {
+        if (index % 2 === 0) {
+          el.push(tempDeck.splice(0, 7));
+        } else {
+          el.push(tempDeck.splice(0, 6));
+        }
+        return vm.decks.base;
+      });
+      console.log(tempDeck);
+    },
+  },
+  computed: {
+    // 亂數產生牌
+    shuffledCard() {
+      // 快速產生 1 ~ 52 的陣列
+      const tempArray = Array.from(new Array(52)).map((el, key) => key + 1);
+      // Fisher–Yates shuffle 演算法(打亂牌組)
+      const shuffle = (array) => {
+        for (let i = array.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+      };
+      return shuffle(tempArray);
+    },
+  },
+  mounted() {
+    // console.log(this.shuffledCard);
+    this.deal();
+    console.log(this.decks);
+  },
 };
 </script>
 
