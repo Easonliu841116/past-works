0c0fcb83e2a7fffc5f51c05652b4984844ac37e1
Eason Liu <a0956031116@gmail.com>
2019-07-29 02:17:04 +0800

    add cover

--- project-freecell/src/components/Index.vue
+++ project-freecell/src/components/Index.vue
@@ -1,42 +1,51 @@
 <template>
   <div>
-    <div class="c-main-container mx-auto">
-      <div class="logo">
-        <h1>Freecell</h1>
+    <div class="c-cover-container">
+      <div class="c-enter-panel mx-auto">
+        <div class="c-cover-logo-secondary"></div>
+        <h1 class="c-cover-logo mt-4 mb-5"></h1>
+        <button class="c-btn-enter" @click="startGame">開始遊戲</button>
       </div>
-      <div class="settings-container">
-        <a href="#" @click.prevent class="setting bg-reset">重玩</a>
-        <a href="#" @click.prevent class="setting bg-pre_step">上一步</a>
-        <a href="#" @click.prevent class="setting bg-pause">暫停</a>
-      </div>
-      <div class="c-decks-container">
-        <div class="c-deck-panel" id="blank-deck">
-          <div class="c-blank-deck-border c-card c-bg-deck" v-for="(item, key) in 4" :key="key">
-          </div>
+    </div>
+    <div class="c-main-container">
+      <div class="c-main-wrapper mx-auto">
+        <div class="c-main-logo">
+          <h2>Freecell</h2>
         </div>
-        <div class="c-deck-panel" id="target-deck">
-          <div class="c-target-deck-shadow c-card" v-for="(item, key) in 4" :key="key"
-          :class="{['c-target' + (key + 1)]: true}">
-          </div>
+        <div class="c-settings-container">
+          <a href="#" @click.prevent class="c-setting c-bg-reset">重玩</a>
+          <a href="#" @click.prevent class="c-setting c-bg-pre_step">上一步</a>
+          <a href="#" @click.prevent class="c-setting c-bg-pause">暫停</a>
         </div>
-      </div>
-      <div class="c-cards-container">
-        <div class="c-cards-panel">
-          <div class="c-card-column" v-for="(item, itemKey) in 4" :key="itemKey">
-            <div v-for="(el, elKey) in decks.base[item - 1]" :key="elKey">
-              <div class="c-card c-cards-border c-card-position-absolute"
-              v-for="(card, cardKey) in el" :key="cardKey"
-                :class="{[cardKey + 1]: true}" :id="card">
-              </div>
+        <div class="c-decks-container">
+          <div class="c-deck-panel" id="blank-deck">
+            <div class="c-blank-deck-border c-card c-bg-deck" v-for="(item, key) in 4" :key="key">
+            </div>
+          </div>
+          <div class="c-deck-panel" id="target-deck">
+            <div class="c-target-deck-shadow c-card" v-for="(item, key) in 4" :key="key"
+            :class="{['c-target' + (key + 1)]: true}">
             </div>
           </div>
         </div>
-        <div class="c-cards-panel">
-          <div class="c-card-column" v-for="(item, itemKey) in 4" :key="itemKey">
-            <div v-for="(el, elKey) in decks.base[item + 3]" :key="elKey">
-              <div class="c-card c-cards-border c-card-position-absolute"
-              v-for="(card, cardKey) in el" :key="cardKey"
-                :class="{[cardKey + 1]: true}" :id="card">
+        <div class="c-cards-container">
+          <div class="c-cards-panel">
+            <div class="c-card-column" v-for="(item, itemKey) in 4" :key="itemKey">
+              <div v-for="(el, elKey) in decks.base[item - 1]" :key="elKey">
+                <div class="c-card c-cards-border c-card-position-absolute"
+                v-for="(card, cardKey) in el" :key="cardKey"
+                  :class="{[cardKey + 1]: true}" :id="card">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="c-cards-panel">
+            <div class="c-card-column" v-for="(item, itemKey) in 4" :key="itemKey">
+              <div v-for="(el, elKey) in decks.base[item + 3]" :key="elKey">
+                <div class="c-card c-cards-border c-card-position-absolute"
+                v-for="(card, cardKey) in el" :key="cardKey"
+                  :class="{[cardKey + 1]: true}" :id="card">
+                </div>
               </div>
             </div>
           </div>
@@ -47,6 +56,9 @@
 </template>
 
 <script>
+import $ from 'jquery';
+import 'jquery-ui';
+
 export default {
   data() {
     return {
@@ -117,12 +129,35 @@ export default {
       basePositionArray.forEach((el) => {
         const dom2 = [...document.getElementsByClassName(`${el}`)];
         dom2.forEach((item) => {
-          item.style.top = `${Math.floor((Math.random() - 2) * 1000)}px`;
           setTimeout(() => {
             item.style.top = `${35 * (el - 1)}px`;
-          }, (el) * 75);
+          }, (el) * 150);
+          setTimeout(() => {
+            item.style.transition = 'none';
+            $('.c-card').draggable({
+              containment: '.c-main-container',
+              snap: '.c-card-column',
+            });
+          }, 1500);
         });
       });
+    },
+    startGame() {
+      const vm = this;
+      const cover = document.querySelector('.c-cover-container');
+      const main = document.querySelector('.c-main-container');
+      cover.style.top = '-1000px';
+      cover.style.left = '-1000px';
+      setTimeout(() => {
+        main.style.top = '50%';
+        main.style.left = '50%';
+      }, 750);
+      setTimeout(() => {
+        vm.deal();
+      }, 1000);
+      setTimeout(() => {
+        vm.changeCardStyle();
+      }, 1300);
     },
   },
   computed: {
@@ -139,12 +174,6 @@ export default {
       };
       return shuffle(baseArray);
     },
-  },
-  created() {
-    this.deal();
-  },
-  mounted() {
-    this.changeCardStyle();
   },
 };
 </script>
--- project-freecell/src/main.js
+++ project-freecell/src/main.js
@@ -1,11 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
 import 'jquery';
+import 'jquery-ui/ui/widgets/draggable';
 import 'bootstrap';
-import CardNumber from './filters/CardNumber';
 
 Vue.config.productionTip = false;
-Vue.filter('CardNumberFilter', CardNumber);
 
 new Vue({
   render: h => h(App),
